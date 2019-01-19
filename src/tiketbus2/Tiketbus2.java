a874c72cc4cc0c098028b74c232c4fb225e2a71a
andhealfikha <andheafikha@gmail.com>
2019-01-19 18:55:17 +0700

    first commit

---
+++ src/tiketbus2/Tiketbus2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiketbus2;
+
+/**
+ *
+ * @author Andhea
+ */
+public class Tiketbus2 {
+    
+    
+    public static void main(String[] args){
+        tiket_bus2 Laund = new tiket_bus2();
+        Laund.setVisible(true);
+    }
+}
